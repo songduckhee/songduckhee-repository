8f1ef823d2fc1904cd87db20d4c1c70e7374d016
songduckhee <vhaqjvl1@gmail.com>
2025-11-13 16:12:35 +0900

    fix : 클라우드연결해제

--- 스파르타 던전 탐험 만들기/Assets/Scripts/Trampoline.cs
+++ 스파르타 던전 탐험 만들기/Assets/Scripts/Trampoline.cs
@@ -18,6 +18,7 @@ public class Trampoline : MonoBehaviour
 	private void OnCollisionEnter(Collision collision)
 	{
 		playerRigidbody = collision.rigidbody;
+		Debug
 
 		playerRigidbody.AddForce(transform.up * power);
 		playerRigidbody.AddForce(transform.up * power, ForceMode.Impulse);
